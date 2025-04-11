97172bc25d94010eeeeda0abf3af7d5ed9671eac
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2025-04-11 10:02:15 +0200

    Update classification.py

--- src/otx/core/data/dataset/classification.py
+++ src/otx/core/data/dataset/classification.py
@@ -114,8 +114,7 @@ class OTXMultilabelClsDataset(OTXDataset):
 
         # Torch's one_hot() expects the input to be of type long
         # However, when labels are empty, they are of type float32
-        if labels.dtype != torch.long:
-            labels = labels.long()
+        labels = labels.long()
         onehot = functional.one_hot(labels, self.num_classes).sum(0).clamp_max_(1)
         if ignored_labels:
             for ignore_label in ignored_labels:
