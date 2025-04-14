377724a17859a713d531548c87646e59822cf263
Rajesh Gangireddy <rajesh.gangireddy@intel.com>
2025-04-14 15:04:45 +0200

    Update hlabel.py

--- src/training_scripts/classification_scripts/hlabel.py
+++ src/training_scripts/classification_scripts/hlabel.py
@@ -2,7 +2,7 @@ from src.otx.engine import Engine
 import time
 
 recipe_full_path = "/home/rgangire/workspace/code/repos/Geti-Labs/OTX/repo/training_extensions/src/otx/recipe/classification/h_label_cls/efficientnet_b0.yaml"
-data_path_full = "/home/rgangire/workspace/datasets/Classification/Processed/Stanford-cars-hlabel_500_50_100"
+data_path_full = "/home/rgangire/workspace/datasets/Classification/Processed/CardsDataset-H-Label-Tiny-2L-6N_36_20_100"
 
 engine = Engine.from_config(
           config_path=recipe_full_path,
